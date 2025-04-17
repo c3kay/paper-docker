89aa96844d8d205a2b1a614683e774d62ddf5635
Calvin <c3kay@icloud.com>
2025-04-17 21:20:16 +0200

    updated paper download script

--- install-paper.sh
+++ install-paper.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env sh
+# https://docs.papermc.io/misc/downloads-api
 set -e
 
-# https://docs.papermc.io/misc/downloads-api
-: "${MC_VERSION:=$(curl -s 'https://api.papermc.io/v2/projects/paper' | jq -r '.versions[-1]')}"
-: "${BUILD:=$(curl -s "https://api.papermc.io/v2/projects/paper/versions/${MC_VERSION}/builds" | jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')}"
+get_latest_stable_version() {
+  latest_versions=$(curl -fs https://api.papermc.io/v2/projects/paper | jq -r '.versions | reverse[]')
 
-if [ "$BUILD" != "null" ]; then
-    JAR_NAME="paper-${MC_VERSION}-${BUILD}.jar"
-    PAPERMC_URL="https://api.papermc.io/v2/projects/paper/versions/${MC_VERSION}/builds/${BUILD}/downloads/${JAR_NAME}"
-    echo "$PAPERMC_URL"
-    curl -o /opt/minecraft/server.jar "$PAPERMC_URL"
-else
-    echo "No stable build for version ${MC_VERSION} found!"
-    exit 1
-fi
+  while IFS= read -r cur_version; do
+    has_stable=$(curl -fs "https://api.papermc.io/v2/projects/paper/versions/${cur_version}/builds" | jq '[.builds[] | select(.channel == "default")] | length > 0')
+
+    if [ "$has_stable" = "true" ]; then
+      echo "$cur_version"
+      return 0
+    fi
+  done <<< "$latest_versions"
+
+  echo "Error: No stable PaperMC version found." >&2
+  return 1
+}
+
+version="${MC_VERSION:-$(get_latest_stable_version)}"
+build=$(curl -s "https://api.papermc.io/v2/projects/paper/versions/${version}/builds" | jq '.builds | .[-1] | .build')
+echo "Latest build for MC ${version}: ${build}"
+
+jar_url="https://api.papermc.io/v2/projects/paper/versions/${version}/builds/${build}/downloads/paper-${version}-${build}.jar"
+echo "Downloading from: ${jar_url}"
+curl -o "/opt/minecraft/server.jar" "$jar_url"
